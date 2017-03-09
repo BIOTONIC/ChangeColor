aee8f628036878ccdcc7e8bf80d2fd22bfa30a4c
lovejoy <805197013@qq.com>
2017-03-09 09:03:22 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/ChangeColor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChangeColor.dir/colorChanging.cpp.o"
+  "ChangeColor.pdb"
+  "ChangeColor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChangeColor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
